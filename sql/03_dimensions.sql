21ca07100f37dade3c65d8dfb67e4c6ff08b08f6
Bonginkosi Khoza <bonginkosibonginkosi29@gmail.com>
2025-12-26 23:05:56 +0200

    created dim_product with clean logic

--- sql/03_dimensions.sql
+++ sql/03_dimensions.sql
@@ -1,32 +1,19 @@
+
 -- =====================================================================================
--- DIMENSION: DATE
--- =====================================================================================
-
-DROP TABLE IF EXISTS dim_date;
-
-CREATE TABLE dim_date AS 
-SELECT DISTINCT
-    DATE(REPLACE(SUBSTR(InvoiceDate, 1, 10), '/', '-')) AS date_value, 
-    CAST(STRFTIME('%Y', REPLACE(SUBSTR(InvoiceDate, 1, 10), '/', '-')) AS INTEGER) AS year,
-    CAST(STRFTIME('%m', REPLACE(SUBSTR(InvoiceDate, 1, 10), '/', '-'))AS INTEGER) AS month,
-    CAST(STRFTIME('%d', REPLACE(SUBSTR(InvoiceDate, 1, 10), '/', '-')) AS INTEGER) AS day,
-    STRFTIME('%Y-%m', REPLACE(SUBSTR(InvoiceDate, 1, 10), '/', '-')) AS year_month
-FROM stg_sales
-WHERE InvoiceDate IS NOT NULL;
-
--- =======
 --  DIMENSION: PRODUCT
+-- ====================================================================================
 
 DROP TABLE IF EXISTS dim_product;
 
 CREATE TABLE dim_product AS
-SELECT
-    ROW_NUMBER() OVER (ORDER BY StockCode) AS product_key,
-    StockCode,
-    COALESCE(TRIM(UPPER(Description)), 'UKNOWN PRODUCT') AS product_name
-FROM (
-    SELECT DISTINCT StockCode, Description
-    FROM stg_sales
-    WHERE StockCode IS NOT NULL
-    GROUP BY StockCode
-);
+SELECT DISTINCT
+    StockCode AS product_code,
+    COALESCE(NULLIF(TRIM(Description), ''), 'UNKNOWN PRODUCT') AS product_name
+FROM stg_sales;
+------------------null / blank description handled replaced with "UNKNOWN PRODUCT"
+------------------using COALESCE= to return first non-null value
+
+-- =====================================================================================
+-- DIMENSION: DATE
+-- =====================================================================================
+
